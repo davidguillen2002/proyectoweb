cf31097ebef4afb5fe17623cf9a87b459ff6541e
davidguillen2002 <davidanguillen@gmail.com>
2023-09-29 14:38:29 -0500

    LOGIN

--- base/views.py
+++ base/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from .models import Tarea
 
+
+class Logueo(LoginView):
+    template_name = "base/login.html"
+    field = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tareas')
 
 class ListaPendientes(ListView):
     model = Tarea
@@ -33,3 +42,6 @@ class EliminarTarea(DeleteView):
     model = Tarea
     context_object_name = 'tareas'
     success_url = reverse_lazy('tareas')
+
+
+
