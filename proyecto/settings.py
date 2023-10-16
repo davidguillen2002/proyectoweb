fd0638a9eeefbb8d0b547616ba09e5b8abdc0f05
davidguillen2002 <davidanguillen@gmail.com>
2023-10-16 13:22:43 -0500

    REQUIREMENTS 4

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -24,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@yxi$-yckg4^%rh7xlkzxyhb&62*93&$&cqflhjpj3qs+_j2%s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['auto-seguros.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
@@ -124,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
